d8a7929e489bbe839d30c35d0d5cb38320b7cacf
Endesapt <103508859+Endesapt@users.noreply.github.com>
2024-08-09 15:16:45 +0300

    added several dots

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = 2021;
 const app = express();
 app.get('/', (req, res) => {
     console.log(req.url)
-    res.send('A Simple Node.js Server is Up and Running.......')
+    res.send('A Simple Node.js Server is Up and Running.........')
 })
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
