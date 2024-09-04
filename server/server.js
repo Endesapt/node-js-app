5eb97030f1129ba6a61cadcbea6eec807084798a
Matvei Verzhbitski <103508859+Endesapt@users.noreply.github.com>
2024-09-04 14:45:55 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = 2021;
 const app = express();
 app.get('/', (req, res) => {
     console.log(req.url)
-    res.send('A Simple Node.js Server is Up and Runningg again')
+    res.send('A Simple Node.js Server is Up and Runningg again again')
 })
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`)
